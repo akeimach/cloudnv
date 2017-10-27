fb85eb3b166401063617e747b8b2bdc03ffb13e4
asajbel <serdan66@gmail.com>
2017-10-26 20:22:05 -0700

    Able to read and upload a file to imgur getting back a url

--- assets/javascript/imgur.js
+++ assets/javascript/imgur.js
@@ -1,70 +1,53 @@
-var imgur_id = "cffdbdcf9cb88c7";
-var imgur_sec = "4e806c50fb260cc521bfe11d4e7edfa22cfbf684";
+ function gotImage(event) {
+  var reader = event.target;
+   $("#displayImage").attr("src", reader.result);
+   var base64result = reader.result.split(',')[1];
+   var form = new FormData();
+   form.append("image", base64result);
 
-authURL = "https://api.imgur.com/oauth2/authorize?client_id="+imgur_id+"&response_type=token"
+   var settings = {
+     "async": true,
+     "crossDomain": true,
+     "url": "https://api.imgur.com/3/image",
+     "method": "POST",
+     "headers": {
+       "authorization": `Client-ID ${apiKey.imgur_client_id}`
+     },
+     "processData": false,
+     "contentType": false,
+     "mimeType": "multipart/form-data",
+     "data": form
+   }
 
-// $.ajax({
-//   "url": authURL,
-//   "method": "GET"
-// }).done(function (response){
-//   console.log(response);
-// });
+   $.ajax(settings).done(function(response) {
+     var res = JSON.parse(response);
+     console.log(res.data.link);
+   });
+ }
 
-var token = ""
+ function  readPic(event)  {
+   console.log(event);
+   console.log($(this));
+   var file = this.files[0];
+   var reader = new FileReader();
+   reader.onloadend = gotImage;
+   reader.readAsDataURL(file);
 
-var form = new FormData();
-form.append("refresh_token", "4c771b1e1055e3eb3adee452a2155ebd258392d1");
-form.append("client_id", "cffdbdcf9cb88c7");
-form.append("client_secret", "4e806c50fb260cc521bfe11d4e7edfa22cfbf684");
-form.append("grant_type", "refresh_token");
+ }
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.imgur.com/oauth2/token",
-  "method": "POST",
-  "headers": {},
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
-}
+ $(document).ready(function addUpload() {
 
+   var imgur_client_id = "cffdbdcf9cb88c7";
+   var imgur_sec = "4e806c50fb260cc521bfe11d4e7edfa22cfbf684";
+   var authURL = "https://api.imgur.com/oauth2/authorize?client_id=" + imgur_client_id + "&response_type=token"
+   var token = ""
 
+   var form = new FormData();
+   form.append("refresh_token", "4c771b1e1055e3eb3adee452a2155ebd258392d1");
+   form.append("client_id", "cffdbdcf9cb88c7");
+   form.append("client_secret", "4e806c50fb260cc521bfe11d4e7edfa22cfbf684");
+   form.append("grant_type", "refresh_token");
 
-$.ajax(settings).done(function (response) {
-  var res = JSON.parse(response);
-  console.log(JSON.parse(response));
-  token = res.access_token;
-  console.log(token);
+   $("#uploadBtn").on("change", readPic);
 
-  var accountGet = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.imgur.com/3/account/voidreturner",
-  "method": "GET",
-  "headers": {"authorization": `Bearer ${token}`},
-}
-console.log(accountGet.headers);
-  $.ajax(accountGet).done(function (response){
-    console.log(response);
-  })
-$.ajax( {
-  "url": "https://api.imgur.com/3/account/me/images",
-  "meathod": "GET",
-  "headers": {"authorization": `Bearer ${token}`}
-}).done (function (response) {
-  console.log(response);
-});
-});
-
-$.ajax( {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.imgur.com/3/account/images",
-  "meathod": "GET",
-  "headers": {"authorization": `Client-ID ${imgur_id}`}
-}).done (function (response) {
-  console.log("Client-ID:");
-  console.log(response);
-});
+ });
---
+++ assets/javascript/keys.js
@@ -0,0 +1,4 @@
+var apiKey = {
+    vision: "AIzaSyCx1GhLhN-MJGgS4uItlichD2gxgtWQag8",
+    imgur_client_id: "cffdbdcf9cb88c7"
+}
