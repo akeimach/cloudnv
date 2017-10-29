395ee0dc15d706916e0cd22904c71c190a2d9cb8
asajbel <serdan66@gmail.com>
2017-10-29 14:24:26 -0700

    load image library added to fix uploaded image incorrect orientation

--- assets/javascript/imgur.js
+++ assets/javascript/imgur.js
@@ -4,30 +4,36 @@
 //Puts image in the DOM on an element with the Id displayImage
 function gotImage(event) {
   var reader = event.target;
-  displayPic(reader.result);
-  var base64result = reader.result.split(',')[1];
-  queryVisionAPI(base64result);
-  var form = new FormData();
-  form.append("image", base64result);
+  // displayPic(reader.result);
+  var base64result = reader.result.replace(/^data:image\/[a-z]+;base64,/, "");
+  if (base64result.length < 5333333) {
+    console.log("Image is less than 4MB")
+    queryVisionAPI(base64result);
+  } else {
+    console.log("Image is greater than 4MB, wait for Imgur")
+    var form = new FormData();
+    form.append("image", base64result);
 
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.imgur.com/3/image",
-    "method": "POST",
-    "headers": {
-      "authorization": `Client-ID ${apiKey.imgur_client_id}`
-    },
-    "processData": false,
-    "contentType": false,
-    "mimeType": "multipart/form-data",
-    "data": form
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.imgur.com/3/image",
+      "method": "POST",
+      "headers": {
+        "authorization": `Client-ID ${apiKey.imgur_client_id}`
+      },
+      "processData": false,
+      "contentType": false,
+      "mimeType": "multipart/form-data",
+      "data": form
+    }
+
+    $.ajax(settings).done(function(response) {
+      var res = JSON.parse(response);
+      queryVisionAPI(res.data.link);
+      console.log(res.data.link);
+    });
   }
-
-  $.ajax(settings).done(function(response) {
-    var res = JSON.parse(response);
-    console.log(res.data.link);
-  });
 }
 
 //Displays picture on the page
@@ -41,12 +47,35 @@ function displayPic(picture) {
 //Make sure that a file is an image and if so sends to the FileReader
 //file is a valid file blob
 function readPic(file) {
+  console.log(file);
   var fileType = file["type"].split("/")[0];
   if (fileType !== "image") {
     console.log("Not an image")
   } else {
+    loadImage.parseMetaData(file, function(data) {
+      //default image orientation
+      var orientation = 0;
+      //if exif data available, update orientation
+      if (data.exif) {
+        orientation = data.exif.get('Orientation');
+      }
+      var loadingImage = loadImage(
+        file,
+        function(canvas) {
+          //here's the base64 data result
+          var base64data = canvas.toDataURL('image/jpeg');
+          //here's example to show it as on imae preview
+          var img_src = base64data.replace(/^data\:image\/\w+\;base64\,/, '');
+          displayPic(base64data);
+        }, {
+          //should be set to canvas : true to activate auto fix orientation
+          canvas: true,
+          orientation: orientation
+        }
+      );
+    });
     var reader = new FileReader();
-    reader.onloadend = gotImage;
+    reader.onload = gotImage;
     reader.readAsDataURL(file);
 
   }
@@ -57,26 +86,35 @@ $(document).ready(function addUpload() {
   var imgur_client_id = "cffdbdcf9cb88c7";
   var imgur_sec = "4e806c50fb260cc521bfe11d4e7edfa22cfbf684";
   var authURL = "https://api.imgur.com/oauth2/authorize?client_id=" + imgur_client_id + "&response_type=token"
-  var token = ""
+  var auth_token = ""
 
-  var form = new FormData();
-  form.append("refresh_token", "4c771b1e1055e3eb3adee452a2155ebd258392d1");
-  form.append("client_id", "cffdbdcf9cb88c7");
-  form.append("client_secret", "4e806c50fb260cc521bfe11d4e7edfa22cfbf684");
-  form.append("grant_type", "refresh_token");
+  var refreshForm = new FormData();
+  refreshForm.append("refresh_token", "4c771b1e1055e3eb3adee452a2155ebd258392d1");
+  refreshForm.append("client_id", "cffdbdcf9cb88c7");
+  refreshForm.append("client_secret", "4e806c50fb260cc521bfe11d4e7edfa22cfbf684");
+  refreshForm.append("grant_type", "refresh_token");
 
-  $("#drag").on('dragenter', function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    $(this).css('border', '2px solid #0B85A1');
-  });
   $("#drag").on('dragover', function(e) {
     e.stopPropagation();
     e.preventDefault();
+    console.log('dragover');
+    $(this).css('border', '2px solid #66aede');
+    $(this).css('background-color', 'rgba(255,255,255,0.6)');
+    $(this).css('color', '#173848');
+  });
+  $("#drag").on('dragleave', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('dragleave');
+    $(this).css('border', '2px dotted #66aede');
+    $(this).css('background-color', 'rgba(255,255,255,0.4)');
+    $(this).css('color', '#31708f');
   });
   $("#drag").on('drop', function(e) {
 
-    $(this).css('border', '2px dotted #0B85A1');
+    $(this).css('border', '2px dotted #66aede');
+    $(this).css('background-color', 'rgba(255,255,255,0.4)');
+    $(this).css('color', '#31708f');
     e.preventDefault();
     var files = e.originalEvent.dataTransfer.files;
 
