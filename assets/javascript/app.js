1334de8a61f58ac3e23c084e245fbc5babeb0f18
AKmach <akeimach@users.noreply.github.com>
2017-10-29 23:14:47 -0700

    rewrite cloud word search, now more robust algorithm

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,40 @@
 
 
 function isURL(imageData) {
-    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?'+ // port
-    '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
-    '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    var urlPattern = new RegExp("^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name and extension
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+    "(\\:\\d+)?" + // port
+    "(\\/[-a-z\\d%@_.~+&:]*)*" + // path
+    "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" + // query string
+    "(\\#[-a-z\\d_]*)?$","i"); // fragment locator
     return urlPattern.test(imageData);
 }
 
 
 
 function getCloudDescriptor(visionResults) {
-    for (var i = 0; i < cloudWordsArray.length; i++) {
-        // if the cloud word in the array exists in the google vision results
-        if (visionResults.indexOf(cloudWordsArray[i]) !== -1) {
-            // set the cloud word
-            console.log("chosen word: " + cloudWordsArray[i]);
-            return cloudWordsArray[i];
+    var searchString = [];
+    for (var i = 0; i < visionResults.length; i++) {
+        if ((cloudWordsArray.indexOf(visionResults[i]) !== -1) && (searchString.length < 2)) {
+            searchString.push(visionResults[i]);
         }
+    }
+    if (searchString.length === 1) {
+        // add the word 'cloud' if only one specific descriptor found
+        searchString.push("cloud");
+    }
+    if (searchString.length === 0) {
+        // if no specific descriptors were found, check the broad categories
+        for (var i = 0; i < visionResults.length; i++) {
+            if ((cloudTypesArray.indexOf(visionResults[i]) !== -1) && (searchString.length < 1)) {
+                searchString.push(visionResults[i]);
+            }
+        }
+    }
+    if (searchString.length > 0) {
+        console.log("chosen words: " + searchString.join(" "));
+        return searchString.join(" ");
     }
     $("#cloudDesc").attr("hidden", false);
     $("#cloudDesc").empty();
@@ -50,6 +64,7 @@ function parseWikiAPI(pageID) {
         var removeBracket = removeImage.replace(/\[\[[\w\s]+\||\]\]|\[\[/g, "");
         var content = removeBracket.replace(/&nbsp;/g, " ");
         content = content.replace(/''/g, "\"");
+        content = content.replace(/__NOTOC__/g, "");
 
         $("#cloudDesc").attr("hidden", false);
         $("#cloudDesc").empty();
@@ -120,27 +135,20 @@ function queryVisionAPI(imageData) {
             console.log(result.responses[0].error);
         }
         else {
-            var resultWebDetect = result.responses[0].webDetection.webEntities;
-            var resultLabels = result.responses[0].labelAnnotations;
-            console.log("vision web detect results", resultWebDetect);
-            console.log("vision label detect results", resultLabels);
-            for (var i = 0; i < resultWebDetect.length; i++) {
-                var resultWord = resultWebDetect[i].description;
+            var queryResults = result.responses[0].webDetection.webEntities.concat(result.responses[0].labelAnnotations);
+            for (var i = 0; i < queryResults.length; i++) {
+                var resultWord = queryResults[i].description;
                 if (resultWord) {
                     resultWord = resultWord.toLowerCase();
-                    resultWord.replace(" cloud", "");
-                    visionResults.push(resultWord);
+                    var wordArray = resultWord.split(" ");
+                    for (var j = 0; j < wordArray.length; j++) {
+                        if (visionResults.indexOf(wordArray[j]) === -1) {
+                            visionResults.push(wordArray[j]);
+                        }
+                    }
                 }
             }
-            for (var i = 0; i < resultLabels.length; i++) {
-                var resultWord = resultLabels[i].description;
-                if (resultWord) {
-                    resultWord = resultWord.toLowerCase();
-                    resultWord.replace(" cloud", "");
-                    visionResults.push(resultWord);
-                }
-            }
-            console.log("image description results", visionResults);
+            console.log(visionResults);
             var searchString = getCloudDescriptor(visionResults);
             if (searchString) {
                 // if the cloud type was found in our cloud word array
@@ -165,163 +173,69 @@ $("#submit").on("click", function(event) {
 
 
 
+var cloudTypesArray = [
+    "stratus",
+    "stratocumulus",
+    "altocumulus",
+    "cirrus",
+    "cirrocumulus",
+    "cirrostratus",
+    "cumulonimbus",
+    "altostratus",
+    "nimbostratus",
+    "cumulus"
+];
 
 var cloudWordsArray = [
-    "cumulus humilis radiatus",
-    "cumulus mediocris radiatus",
-    "cumulus congestus radiatus",
-    "cumulus fractus radiatus",
-    "stratus nebulosus opacus",
-    "stratus nebulosus translucidus",
-    "stratus nebulosus undulatus",
-    "stratus fractus opacus",
-    "stratus fractus translucidus",
-    "stratus fractus undulatus",
-    "stratocumulus stratiformis translucidus",
-    "stratocumulus stratiformis perlucidus",
-    "stratocumulus stratiformis opacus",
-    "stratocumulus stratiformis duplicatus",
-    "stratocumulus stratiformis undulatus",
-    "stratocumulus stratiformis radiatus",
-    "stratocumulus stratiformis lacunosus",
-    "stratocumulus lenticularis translucidus",
-    "stratocumulus lenticularis perlucidus",
-    "stratocumulus lenticularis opacus",
-    "stratocumulus lenticularis duplicatus",
-    "stratocumulus lenticularis undulatus",
-    "stratocumulus lenticularis radiatus",
-    "stratocumulus lenticularis lacunosus",
-    "stratocumulus castellanus translucidus",
-    "stratocumulus castellanus perlucidus",
-    "stratocumulus castellanus opacus",
-    "stratocumulus castellanus duplicatus",
-    "stratocumulus castellanus undulatus",
-    "stratocumulus castellanus radiatus",
-    "stratocumulus castellanus lacunosus",
-    "altocumulus stratiformus translucidus",
-    "altocumulus stratiformus perlucidus",
-    "altocumulus stratiformus opacus",
-    "altocumulus stratiformus duplicatus",
-    "altocumulus stratiformus undulatus",
-    "altocumulus stratiformus radiatus",
-    "altocumulus stratiformus lacunosus",
-    "altocumulus lenticularis translucidus",
-    "altocumulus lenticularis perlucidus",
-    "altocumulus lenticularis opacus",
-    "altocumulus lenticularis duplicatus",
-    "altocumulus lenticularis undulatus",
-    "altocumulus lenticularis radiatus",
-    "altocumulus lenticularis lacunosus",
-    "altocumulus castellanus translucidus",
-    "altocumulus castellanus perlucidus",
-    "altocumulus castellanus opacus",
-    "altocumulus castellanus duplicatus",
-    "altocumulus castellanus undulatus",
-    "altocumulus castellanus radiatus",
-    "altocumulus castellanus lacunosus",
-    "altocumulus floccus translucidus",
-    "altocumulus floccus perlucidus",
-    "altocumulus floccus opacus",
-    "altocumulus floccus duplicatus",
-    "altocumulus floccus undulatus",
-    "altocumulus floccus radiatus",
-    "altocumulus floccus lacunosus",
-    "cirrus fibratus intortus",
-    "cirrus fibratus radiatus",
-    "cirrus fibratus vertebratus",
-    "cirrus fibratus duplicatus",
-    "cirrus uncinus intortus",
-    "cirrus uncinus radiatus",
-    "cirrus uncinus vertebratus",
-    "cirrus uncinus duplicatus",
-    "cirrus spissatus intortus",
-    "cirrus spissatus radiatus",
-    "cirrus spissatus vertebratus",
-    "cirrus spissatus duplicatus",
-    "cirrus castellanus intortus",
-    "cirrus castellanus radiatus",
-    "cirrus castellanus vertebratus",
-    "cirrus castellanus duplicatus",
-    "cirrus floccus intortus",
-    "cirrus floccus radiatus",
-    "cirrus floccus vertebratus",
-    "cirrus floccus duplicatus",
-    "cirrocumulus stratiformis undulatus",
-    "cirrocumulus stratiformis lacunosus",
-    "cirrocumulus lenticularis undulatus",
-    "cirrocumulus lenticularis lacunosus",
-    "cirrocumulus castellanus undulatus",
-    "cirrocumulus castellanus lacunosus",
-    "cirrocumulus floccus undulatus",
-    "cirrocumulus floccus lacunosus",
-    "cirrostratus fibratus duplicatus",
-    "cirrostratus fibratus undulatus",
-    "cirrostratus nebulosus duplicatus",
-    "cirrostratus nebulosus undulatus",
-    "cumulus humilis",
-    "cumulus mediocris",
-    "cumulus congestus",
-    "cumulus fractus",
-    "cumulonimbus calvus",
-    "cumulonimbus capillatus",
-    "stratus nebulosus",
-    "stratus fractus",
-    "stratocumulus stratiformis",
-    "stratocumulus lenticularis",
-    "stratocumulus castellanus",
-    "altocumulus stratiformus",
-    "altocumulus lenticularis",
-    "altocumulus castellanus",
-    "altocumulus floccus",
-    "cirrus fibratus",
-    "cirrus uncinus",
-    "cirrus spissatus",
-    "cirrus castellanus",
-    "cirrus floccus",
-    "cirrostratus fibratus",
-    "cirrostratus nebulosus",
-    "cirrocumulus stratiformis",
-    "cirrocumulus lenticularis",
-    "cirrocumulus castellanus",
-    "cirrocumulus floccus",
-    "altostratus translucidus",
-    "altostratus perlucidus",
-    "altostratus opacus",
-    "altostratus duplicatus",
-    "altostratus undulatus",
-    "altostratus radiatus",
-    "cirrostratus cirrocumulogenitus",
-    "cirrostratus cumulonimbogenitus",
-    "cirrostratus homogenitus",
-    "cirrostratus cirromutatus",
-    "cirrostratus cirrocumulomutatus",
-    "cirrostratus altostratomutatus",
-    "cirrus cirrocumulogenitus",
-    "cirrus cirrostratomutatus",
-    "cirrus altocumulogenitus",
-    "cirrus cumulonimbogenitus",
-    "cirrus homogenitus",
+    "humilis",
+    "radiatus",
+    "mediocris",
+    "congestus",
+    "fractus",
+    "nebulosus",
+    "opacus",
+    "translucidus",
+    "undulatus",
+    "stratiformis",
+    "perlucidus",
+    "duplicatus",
+    "lacunosus",
+    "lenticularis",
+    "castellanus",
+    "stratiformus",
+    "floccus",
+    "fibratus",
+    "intortus",
+    "vertebratus",
+    "uncinus",
+    "spissatus",
+    "calvus",
+    "capillatus",
+    "cirrocumulogenitus",
+    "cumulonimbogenitus",
+    "homogenitus",
+    "cirromutatus",
+    "cirrocumulomutatus",
+    "altostratomutatus",
+    "cirrostratomutatus",
+    "altocumulogenitus",
     "stratocumuliform",
-    "stratocumuliform lacunosus",
-    "cirrocumulus homogenitus",
-    "cirrocumulus cirromutatus",
-    "cirrocumulus cirrostratomutatus",
-    "cirrocumulus altocumulomutatus",
-    "cumuliform floccus",
-    "altocumulus volutus",
+    "altocumulomutatus",
+    "cumuliform",
+    "volutus",
     "iridescence",
-    "circumhorizontal arc",
-    "mutatus mother",
-    "genitus mother",
-    "kelvin-helmholtz wave",
-    "kelvin–helmholtz instability",
-    "mackerel sky",
-    "fallstreak hole",
-    "cumulonimbus incus",
+    "circumhorizontal",
+    "arc",
+    "mutatus",
+    "mother",
+    "genitus",
+    "kelvin–helmholtz",
+    "mackerel",
+    "fallstreak",
+    "incus",
     "anvil",
     "mushroom",
     "arcus",
-    "incus",
     "mamma",
     "mammatus",
     "pannus",
@@ -330,7 +244,8 @@ var cloudWordsArray = [
     "tuba",
     "velum",
     "virga",
-    "aircraft contrails",
+    "aircraft",
+    "contrails",
     "noctilucent",
     "cirriform",
     "columnar",
@@ -338,44 +253,10 @@ var cloudWordsArray = [
     "stratiform",
     "lenticular",
     "asperitas",
-    "stratospheric nacreous",
-    "polar stratospheric",
+    "stratospheric",
     "nacreous",
+    "polar",
     "non-nacreous",
-    "fibratus",
-    "nebulosus",
-    "stratiformis",
-    "lenticularis",
-    "castellanus",
-    "floccus",
-    "uncinus",
-    "spissatus",
-    "stratiformus",
-    "fractus",
-    "capillatus",
-    "calvus",
-    "humilis",
-    "mediocris",
-    "congestus",
-    "radiatus",
-    "opacus",
-    "translucidus",
-    "undulatus",
-    "perlucidus",
-    "duplicatus",
-    "lacunosus",
-    "intortus",
-    "vertebratus",
-    "cumulus",
-    "cumulonimbus",
-    "stratus",
-    "stratocumulus",
-    "altocumulus",
-    "altostratus",
-    "nimbostratus",
-    "cirrus",
-    "cirrocumulus",
-    "cirrostratus"
 ];
 
 
